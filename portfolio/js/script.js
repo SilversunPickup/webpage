e4118b9ea36f0cadcd71cac678886c26f44ebb6a
SilversunPickup <mxbuchner@yahoo.de>
2020-07-27 22:22:26 +0200

    Add files via upload

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -8,10 +8,57 @@ $(document).ready(function() {
 // add fading animation
 
 		animation: 'fade',
-// every 5 s the background img changes
-		
-		play: 5000,
+// every 10 s the background img changes
+
+		play: 10000,
+		pagination: false
 
 	});
-
 });
+	var typed = new Typed(".typed", {
+		strings: ["Data Consultant.", "Business Consultant.", "Predictive Analyst.", "Data Scientist."],
+		typeSpeed: 100,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
+
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        480:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        938:{
+        	items:5
+        }
+   	 }
+	});
+
+	$(function() {
+		$('.chart').easyPieChart({
+			   easing: 'easeInOut',
+			   barColor: '#fff',
+			   trackColor: false,
+			   scaleColor: false,
+			   lineWidth: 4,
+			   size: 152,
+			   onStep: function(from, to, percent) {
+			   		$(this.el).find('').text(Math.round(percent));
+			   }
+
+			    });
+  	  });
+
+
+
+
